ebe2e37abfea7ad5aad664a5300c5bfb6a551874
zhm <2916692001@qq.com>
2026-09-01 19:32:29 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/MYF-63-download/',
   plugins: [react()],
 })
